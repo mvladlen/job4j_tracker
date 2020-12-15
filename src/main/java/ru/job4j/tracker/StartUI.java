c8d1dd0a91889744e86fe02da062620510b61a09
mvladlen <vladlen@ufanet.ru>
2020-12-15 07:33:59 +0500

    9.1 Зависимость от System.out [#414370]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,58 +1,12 @@
 package ru.job4j.tracker;
 
 public class StartUI {
-    public static void createItem(Input input, Tracker tracker) {
-        System.out.println("=== Create a new Item ====");
-        String name = input.askStr("Enter name: ");
-        Item item = new Item(0, name);
-        tracker.add(item);
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
     }
 
-    public static void replaceItem(Input input, Tracker tracker) {
-        System.out.println("=== Edit an Item ====");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
-        String name = input.askStr("Enter name: ");
-        Item item = new Item(id, name);
-        if (tracker.replace(id, item)) {
-            System.out.println("Item replaced");                        // вывод об успешности операции
-        } else {
-            System.out.println("Item not replaced");                        // вывод об ошибке
-        }
-    }
-
-    public static void deleteItem(Input input, Tracker tracker) {
-
-        System.out.println("=== Delete an Item ====");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
-        if (tracker.delete(id)) {
-            System.out.println("Item deleted");                        // вывод об успешности операции
-        } else {
-            System.out.println("Error. Item not deleted");                        // вывод об ошибке
-        }
-
-    }
-
-    public static void findItemById(Input input, Tracker tracker) {
-
-        System.out.println("=== Find an Item by Id ===");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
-        Item item = tracker.findById(id);
-        if (item != null) {
-            System.out.println(item.toString());
-        } else {
-            System.out.println("Заявка с таким id не найдена");
-        }
-    }
-
-    public static void findItemByName(Input input, Tracker tracker) {
-
-        System.out.println("=== Find an Item by name ===");
-        String name = input.askStr("Enter name: ");
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
-            for (Item item : items) System.out.println(item);
-        } else System.out.println("Заявки с таким name не найдены");
-    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -65,20 +19,20 @@ public class StartUI {
     }
 
 
-    private static void showMenu(UserAction[] actions) {
-        System.out.println("Menu.");
+    private void showMenu(UserAction[] actions) {
+        this.out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
-            System.out.println(index + ". " + actions[index].name());
+            this.out.println(index + ". " + actions[index].name());
         }
     }
 
-
     public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(), new ShowAllAction(), new ReplaceAction(), new DeleteAction(), new FindIdAction(), new FindNameAction(), new ExitAction()
+                new CreateAction(output), new ShowAllAction(output), new ReplaceAction(output), new DeleteAction(output), new FindIdAction(output), new FindNameAction(output), new ExitAction(output)
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
 }
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,57 +7,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-    @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC", String.valueOf(1)};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item(0, "Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenReplaceItem() {
-        String[] answers = {"Fix PC", String.valueOf(1), "Bug", String.valueOf(2)};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        StartUI.replaceItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item(2, "Bug");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenReplaceItem2() {
-        Tracker tracker = new Tracker();
-        Item item = new Item(1, "new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
-                "deleted item"
-        };
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertNull(deleted);
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item(1, "new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
-                "replaced item"
-        };
-        StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
+/*
     @Test
     public void whenICreateItem() {
         Input in = new StubInput(
@@ -75,12 +25,10 @@ public class StartUITest {
     @Test
     public void whenIReplaceItem() {
         Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
         Item item = tracker.add(new Item("Replaced item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[]{"0", "1", replacedName /* входные параметры для ReplaceAction */, "1"}
+                new String[]{"0", "1", replacedName , "1"}
         );
         UserAction[] actions = {
                 new ReplaceAction(),
@@ -93,11 +41,9 @@ public class StartUITest {
     @Test
     public void whenIDeleteItem() {
         Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
         Item item = tracker.add(new Item("Deleted item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
         Input in = new StubInput(
-                new String[]{"0", "1" /* входные параметры для DeleteAction */, "1"}
+                new String[]{"0", "1", "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
@@ -106,7 +52,23 @@ public class StartUITest {
         new StartUI().init(in, tracker, actions);
         assertNull(tracker.findById(item.getId()));
     }
-
+ */
+@Test
+public void whenExit() {
+    Output out = new StubOutput();
+    Input in = new StubInput(
+            new String[]{"0"}
+    );
+    Tracker tracker = new Tracker();
+    UserAction[] actions = {
+            new ExitAction(out)
+    };
+    new StartUI(out).init(in, tracker, actions);
+    assertThat(out.toString(), is(
+            "Menu." + System.lineSeparator() +
+                    "0. Exit" + System.lineSeparator()
+    ));
+}
 
 }
 
