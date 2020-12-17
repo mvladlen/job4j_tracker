360c418856ff3c73fca624c2af715ba1301fd495
mvladlen <vladlen@ufanet.ru>
2020-12-17 21:40:25 +0500

    1.1. Тесты на StartUI. [#414397]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -120,4 +120,25 @@ public class StartUITest {
                 "Item{id=1, name='Bug', created"));
     }
 
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"1", "0" /* Пункты меню: неверный, верный.*/}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
 }
