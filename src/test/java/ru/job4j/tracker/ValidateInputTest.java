42907e0fc52bb829ade4ad3ad1a2c5277dab1273
mvladlen <vladlen@ufanet.ru>
2020-12-19 08:24:35 +0500

    2.1. Тест на ValidateInput [#414398] error

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"1", "2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenMultiInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"3", "Vasya", "2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        String name = input.askStr("Enter name:");
+        int id = input.askInt("Enter menu:");
+
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenSubZeroInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
+
+}
+
