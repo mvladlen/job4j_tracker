04e7397118aa7f20d368d500a7b9c715c6ee0c9e
mvladlen <vladlen@ufanet.ru>
2020-12-23 20:43:35 +0500

    0. Коллекции, ArrayList, List, Обобщения [#414357]

---
+++ src/main/java/collection/UsageArrayList.java
@@ -0,0 +1,21 @@
+package collection;
+
+import ru.job4j.tracker.Item;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<Item> ItemNames = new ArrayList<Item>();
+
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
