32ba23c02164717342a14809c739b05c088c3651
mvladlen <vladlen@ufanet.ru>
2021-01-09 10:10:19 +0500

    4. Генерация документации по JavaDoc [#414339]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,58 @@
 package ru.job4j.tracker;
 
+/**
+ * Главный класс для управления меню и объектами
+ */
 public class StartUI {
+    /**
+     * @Output - интерфейс для вывода данных
+     */
     private final Output out;
 
+    /**
+     * Конструктор для меню
+     *
+     * @param out - интерфейс для вывода
+     */
     public StartUI(Output out) {
         this.out = out;
     }
 
+    /**
+     * Главный метод для инициализации объектов и меню
+     *
+     * @param args - не используется
+     */
+    public static void main(String[] args) {
+        Output output = new ConsoleOutput();
+        Input input = new ValidateInput(output, new ConsoleInput());
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(output), new ShowAllAction(output),
+                new ReplaceAction(output), new DeleteAction(output),
+                new FindIdAction(output), new FindNameAction(output),
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+    }
+    /*
+    public void init(Input input, Tracker tracker, UserAction[] actions) {
+        boolean run = true;
+        while (run) {
+            showMenu(actions);
+            int select = Integer.parseInt(input.askStr("Select: "));
+            actions[select].name();
+            run = actions[select].execute(input, tracker);
+        }
+    }*/
+
+    /**
+     * Инициализация обхектов и параметров
+     *
+     * @param input   - интерфейс ввода
+     * @param tracker - объект тракер
+     * @param actions - елементы меню
+     */
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
@@ -20,34 +66,16 @@ public class StartUI {
             run = action.execute(input, tracker);
         }
     }
-    /*
-    public void init(Input input, Tracker tracker, UserAction[] actions) {
-        boolean run = true;
-        while (run) {
-            showMenu(actions);
-            int select = Integer.parseInt(input.askStr("Select: "));
-            actions[select].name();
-            run = actions[select].execute(input, tracker);
-        }
-    }*/
 
+    /**
+     * Метод показа меню
+     *
+     * @param actions - элементы меню
+     */
     private void showMenu(UserAction[] actions) {
         this.out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
             this.out.println(index + ". " + actions[index].name());
         }
-    }
-
-    public static void main(String[] args) {
-        Output output = new ConsoleOutput();
-        Input input = new ValidateInput(output, new ConsoleInput());
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(output), new ShowAllAction(output),
-                new ReplaceAction(output), new DeleteAction(output),
-                new FindIdAction(output), new FindNameAction(output),
-                new ExitAction(output)
-        };
-        new StartUI(output).init(input, tracker, actions);
     }
 }
